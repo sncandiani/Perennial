9e8373b0585dcd1dbcbee52717735da36d287b93
sncandiani <sofiancandiani@gmail.com>
2020-03-23 22:33:41 -0500

    added personalPlants

--- src/components/plants/PlantSearch.js
+++ src/components/plants/PlantSearch.js
@@ -2,10 +2,15 @@ import React, {useState, useEffect} from "react"
 import API from "../../modules/ApiManager"
 
 const PlantSearch = (props) => {
+  
     return ( 
         <>
         <h1>{props.name}</h1>
-        <button className="addPlantButton" type="button" onClick={() => props.createRelationshipObj(props.plantId)}>Add Plant</button>
+        <button className="addPlantButton" type="button" onClick={() => {
+          
+          props.createRelationshipObj(props.plantObj)}
+        
+          }>Add Plant</button>
         {props.userId === props.apiUser ?
         <>
          <button type="button" onClick={() => props.history.push(`plants/${props.plantId}/edit`)}>Edit your listing</button> 
